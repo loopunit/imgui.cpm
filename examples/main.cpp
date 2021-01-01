eb402642d5f12ac7d8542df5987e2acc6aae34d4
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2020-12-31 21:26:17 -0800

    cleanup

--- examples/main.cpp
+++ examples/main.cpp
@@ -20,7 +20,9 @@ int main(int, char**)
 		
                 // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
                 if (show_demo_window)
+				{
                     ImGui::ShowDemoWindow(&show_demo_window);
+				}
 
                 // 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
                 {
@@ -37,7 +39,10 @@ int main(int, char**)
                     ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
 
                     if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					{
                         counter++;
+					}
+
                     ImGui::SameLine();
                     ImGui::Text("counter = %d", counter);
 
@@ -51,7 +56,10 @@ int main(int, char**)
                     ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
                     ImGui::Text("Hello from another window!");
                     if (ImGui::Button("Close Me"))
+					{
                         show_another_window = false;
+					}
+
                     ImGui::End();
                 }
 
