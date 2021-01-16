e3576913a316298907b4fbf95463babc455a98f5
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-15 20:14:23 -0800

    clean up naming convention

--- modules.cpm/basis_universal.cmake
+++ modules.cpm/basis_universal.cmake
@@ -1,8 +1,8 @@
 add_cpm_module(basis_universal)
 
-add_library(basis_universal_cpm STATIC IMPORTED)
-target_include_directories(basis_universal_cpm INTERFACE ${basis_universal_ROOT}/include)
-set_target_properties(basis_universal_cpm PROPERTIES IMPORTED_LOCATION ${basis_universal_ROOT}/lib/basis.lib)
+add_library(basis_universal STATIC IMPORTED)
+target_include_directories(basis_universal INTERFACE ${basis_universal_ROOT}/include)
+set_target_properties(basis_universal PROPERTIES IMPORTED_LOCATION ${basis_universal_ROOT}/lib/basis.lib)
 
 ####
 
