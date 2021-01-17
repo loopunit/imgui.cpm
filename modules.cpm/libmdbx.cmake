45f696b2604c3dce97ff708ee03469719639d4ee
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-16 22:08:01 -0800

    re-enable runtime cpm module

--- modules.cpm/libmdbx.cmake
+++ modules.cpm/libmdbx.cmake
@@ -1,4 +1,6 @@
 add_cpm_module(mdbx FOR_TOOLCHAIN)
 
-#add_cpm_module(libmdbx)
-#include(${mdbx_ROOT}/lib/cmake/HLSLTargets.cmake)
+add_cpm_module(mdbx)
+add_library(mdbx STATIC IMPORTED)
+set_target_properties(mdbx PROPERTIES IMPORTED_LOCATION ${mdbx_ROOT}/lib/mdbx.lib)
+target_include_directories(mdbx INTERFACE ${mdbx_ROOT}/include)
