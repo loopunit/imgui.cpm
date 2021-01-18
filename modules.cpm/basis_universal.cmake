ff9fe2bd9c9b35b8d9f1d2c3d006be5818bd7552
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2021-01-17 23:50:50 -0800

    Namespace more stuff; RelWithDebInfo breaks other builds so revert it, disable mdbx

--- modules.cpm/basis_universal.cmake
+++ modules.cpm/basis_universal.cmake
@@ -1,9 +1,12 @@
-add_cpm_module(basis_universal)
+add_cpm_module(basis_universal FOR_TOOLCHAIN)
 
-add_library(basis_universal STATIC IMPORTED)
-target_include_directories(basis_universal INTERFACE ${basis_universal_ROOT}/include)
-set_target_properties(basis_universal PROPERTIES IMPORTED_LOCATION ${basis_universal_ROOT}/lib/basis.lib)
+add_executable(cpm_toolchain::basisu IMPORTED GLOBAL)
+set_target_properties(cpm_toolchain::basisu PROPERTIES IMPORTED_LOCATION ${basis_universal_ROOT}/bin/basisu.exe)
 
 ####
 
-add_cpm_module(basis_universal FOR_TOOLCHAIN)
+add_cpm_module(basis_universal)
+
+add_library(cpm_runtime::basis_universal STATIC IMPORTED)
+target_include_directories(cpm_runtime::basis_universal INTERFACE ${basis_universal_ROOT}/include)
+set_target_properties(cpm_runtime::basis_universal PROPERTIES IMPORTED_LOCATION ${basis_universal_ROOT}/lib/basis.lib)
--- modules.cpm/glfw.cmake
+++ modules.cpm/glfw.cmake
@@ -1,3 +1,4 @@
 add_cpm_module(glfw)
 
 include(${glfw_ROOT}/lib/cmake/glfw3/glfw3Config.cmake)
+add_library(cpm_runtime::glfw ALIAS glfw)
